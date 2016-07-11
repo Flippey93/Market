e037ce45f5f80aa82fa4f1371b4b1d9cf998ee6c
Flippey <3149365130@qq.com>
2016-07-11 19:48:19 +0800

    actionbar初始化

--- app/src/main/java/com/flippey/market/MainActivity.java
+++ app/src/main/java/com/flippey/market/MainActivity.java
@@ -1,13 +1,51 @@
 package com.flippey.market;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_fl)
+    FrameLayout mLeftMenu;
+    @BindView(R.id.main_drawer_layout)
+    DrawerLayout mDrawerLayout;
+    private ActionBar mActionBar;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initActionBar();
+    }
+
+    private void initActionBar() {
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(R.string.action_title);
+        mActionBar.setDisplayShowHomeEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string
+                .open, R.string.close);
+        mToggle.syncState();
+        mDrawerLayout.setDrawerListener(mToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mToggle.onOptionsItemSelected(item);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/flippey/market/global/MyAppliocation.java
@@ -0,0 +1,14 @@
+package com.flippey.market.global;
+
+import android.app.Application;
+
+/**
+ * @ Author      Flippey
+ * @ Creat Time  2016/7/11 19:47
+ */
+public class MyAppliocation extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
